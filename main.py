3bf97c8729c11c45dfad9fff88cd72c1597e923e
Jakub Jarosiński <dzejkop@DESKTOP-LV2Q3C1.localdomain>
2022-05-29 21:32:43 +0200

    First commit to go

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+# Add phoneCalls.csv to the commit
+
+
 
 if __name__ == "__main__":
     pass
+
+import csv
+
+class MenadzerPolaczen:
+    def ___init__(self,filename):
+        self.filename = filename
+        self.data_dict = self.read_data()
+
+
+    def read_data(self):
+        calls_dictionary = dict()
+        with open(self.filename, 'r') as fin:
+            reader = csv.reader(fin, delimiter= ",")
+            headers = next(reader)
+
+        for row in header:
+            from_subsr = int(row[0])
+             if from_subsr not in calls_dictionary:
+                calls_dictionary[from_subsr] = 0
+            calls_dictionary[from_subsr] += 1
+        return calls_dictionary
+    
+    
+    def pobierz_najczesciej_dzwoniacego(self):
+        return max(self.data_dict.items(), key= lambda x: x[1])
+
+if __name__ == "__main__":
+  print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
+    
